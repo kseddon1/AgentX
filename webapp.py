99a8e6f014802d040d6a3a5431c7183746aa84bd
kirk.seddon@icloud.com <kirk.seddon@icloud.com>
2025-01-28 12:42:28 -0600

    Added pages to easily test different LLM variants

--- webapp.py
+++ webapp.py
@@ -22,10 +22,12 @@ login = authenticator.login('main', key='Login')
 
 if st.session_state["authentication_status"]:
     authenticator.logout('Logout', 'sidebar')
-    web_chat = st.Page("webChat.py", title="Web Chat", icon=":material/add_circle:")
-    web_chat_with_memory = st.Page("webChatWithMemory.py", title="Web Chat with Memory", icon=":material/add_circle:")
+    web_chat_llama = st.Page("webChatWithMemory.py", title="Web Chat (Llama 3.2)", icon=":material/add_circle:")
+    web_chat_deepseek = st.Page("webChatDeepSeek.py", title="Web Chat (DeepSeek-r1)", icon=":material/add_circle:")
+    web_chat_codellama = st.Page("webChatCodeLlama.py", title="Web Chat (CodeLLama)", icon=":material/add_circle:")
+    web_chat_mistral = st.Page("webChatMistral.py", title="Web Chat (CodeLLama)", icon=":material/add_circle:")
     web_sentiment_classifier = st.Page("webClassifier.py", title="Web Classifier", icon=":material/add_circle:")
-    pg = st.navigation([web_chat, web_chat_with_memory,web_sentiment_classifier])
+    pg = st.navigation([web_chat_llama, web_chat_deepseek, web_chat_codellama, web_chat_mistral, web_sentiment_classifier])
     pg.run()
 elif st.session_state["authentication_status"] == False:
     st.error('Username/password is incorrect')

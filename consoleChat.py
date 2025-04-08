a00cd5031ba7865a38651cec58ad6d7c3caf97f7
kseddon1 <kirk.seddon@icloud.com>
2025-04-08 17:18:00 -0500

    Initial sync

--- consoleChat.py
+++
@@ -1,6 +0,0 @@
-#This file is licensed under the MIT No Attribution (MIT-0) License
-
-from AIAgents import ConsoleChatChat, AIAgentFactory
-factory = AIAgentFactory()
-agent = factory.create_agent("console")
-agent.interact()
